911e58c1f5c5daa4dedb32e643d9ad57c8fca129
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-13 03:48:03 +0900

    http request object

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -2,14 +2,33 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("user got into the server");
-  res.writeHead(200, "asdfasdfd", { "content-type": "text/html" });
-  res.write(`<h1>Hello World</h1>`);
+  // console.log(req);
+  console.log(req.method);
+  console.log(req.url);
+
+  const { url } = req;
+
+  if (url === "/") {
+    res.writeHead(200, "asdfasdfd", { "content-type": "text/html" });
+    res.write(`<h1>Hello World</h1>`);
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(`<h1>About Page</h1>`);
+    res.end();
+  } else if (url === "/favicon.ico") {
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write(`<h1>Page Not Found</>`);
+    res.end();
+  }
+
   // res.end(
   //   JSON.stringify({
   //     data: "Hello World!",
   //   })
   // );
-  res.end();
 });
 
 server.listen(5000);
