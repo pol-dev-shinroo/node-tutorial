19cb64155fcd984a7cfa763b3e5f3255f625fb8f
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-15 00:28:02 +0900

    query selector

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -4,7 +4,20 @@ const path = require("path");
 
 const packageJson = require("../react-app/package.json");
 
+const { products, people } = require("./data");
+
 app.use(express.static(path.join(__dirname, "../react-app/build")));
+
+app.get("/api/products", (req, res) => {
+  /** sending the whole json */
+  // res.status(201).json(dataJson);
+  /** 객체 조작 */
+  const newProducts = products.map((item) => {
+    const { id, name, image, price } = item;
+    return { id, name, image, price };
+  });
+  res.json(newProducts);
+});
 
 app.listen(packageJson.port, () => {
   console.log(`listening on port ${packageJson.port} `);
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,24 @@
+import axios from "axios";
+
 function App() {
-  return <>Hello world!!!!</>;
+  const baseURL = "http://localhost:8070";
+
+  const handleProducts = async () => {
+    const res = await axios.get(`${baseURL}/api/products`);
+    console.log(res);
+  };
+
+  const handlePeopel = async () => {
+    const res = await axios.get(`${baseURL}/api/people`);
+    console.log(res);
+  };
+
+  return (
+    <>
+      <button onClick={handleProducts}>Products</button>
+      <button onClick={handlePeopel}>People</button>
+    </>
+  );
 }
 
 export default App;
