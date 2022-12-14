8c2ea820095a8121279aee120d5677406547a579
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-14 16:48:53 +0900

    static files

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,27 +1,18 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log(req);
-  res.status(200).send("Home page");
-});
+app.use(express.static("./navbar-app/public"));
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About page");
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.all("*", (req, res) => {
-  res.status(404).send(`<h1>Error page</h1>`);
+  res.status(400).send("resource not found");
 });
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000");
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
--- 2-express-tutorial/final/02-express-basics.js
+++ 2-express-tutorial/final/02-express-basics.js
@@ -1,2 +1,26 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("resource not found");
+});
+
+app.listen(5000, () => {
+  console.log("listening on port 5000");
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
