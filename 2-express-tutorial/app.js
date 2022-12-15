98763e95b6501bb0790d3fe8b736d32326d255a0
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-16 02:27:09 +0900

    post method - form-urlencoded

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const app = express();
-const { products, people } = require("./data");
-
-console.log(products);
+let { products, people } = require("./data");
+const morgan = require("morgan");
+app.use(morgan("tiny"));
 
 app.use(express.static("./methods-public"));
+// req 보낼때 form-urlencoded 형식으로 보내줘야함 => for parsing form data
+app.use(express.urlencoded({ extended: false }));
+
+// req 보낼때 json형식으로 보내려면....
+app.use(express.json());
 
 app.get("/api/people", (req, res) => {
   res.status(200).json({ res: true, data: people });
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  console.log(typeof req.body);
+  const { name } = req.body;
+  if (name === "") {
+    return res.status(400).send("please provide username");
+  }
+  res.status(200).send(`welcome ${name}`);
 });
 
 app.listen(5000, () => {
