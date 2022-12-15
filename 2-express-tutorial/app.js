45bcaa709183308472a7cc1e3288b6952690c1dc
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-15 21:18:02 +0900

    app.use(logger) -> middleware

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,33 +1,17 @@
 const express = require("express");
 const app = express();
 const packageJson = require("../react-app/package.json");
+const logger = require("./logger");
 
-// req => middleware => res
+app.use(logger);
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  next();
-  // you can also terminate here...
-  // res.send("Testing");
-};
-
-const secondLogger = (req, res, next) => {
-  console.log("second logger");
-  next();
-};
-
-// the second function parameter is middleware (ex.logger)
-// hence it is called the "middleware function"
-app.get("/", logger, secondLogger, (req, res) => {
+app.get("/", (req, res) => {
   res.send("Home");
 });
-app.get("/about", logger, (req, res) => {
+app.get("/about", (req, res) => {
   res.send("About");
 });
 
 app.listen(packageJson.port, () => {
-  console.log("listening on port " + packageJson.port);
+  console.log("listening on " + packageJson.port);
 });
