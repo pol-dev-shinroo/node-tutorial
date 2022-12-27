a75a1f9570ee1dfcf4708aedb31bfe752400de58
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-27 15:05:22 +0900

    setup server

--- 4-store/app.js
+++ 4-store/app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
+/** basic setup */
+app.use(express.json());
+
+/** routes */
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
 /** middleware */
 const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
 app.use(notFound);
+app.use(errorHandler);
 
-app.listen(8000, () => {
-  console.log("listening on port 8000");
-});
+const start = async () => {
+  try {
+    // connect DB
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port 8000");
+    });
+  } catch (err) {}
+};
+
+start();
