7ffc068c1bf71ec8a67c3c385f60e9bd53ca3434
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-29 08:43:38 +0900

    generating token through mongoose middleware

--- 6-jobs-api/controllers/auth.js
+++ 6-jobs-api/controllers/auth.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 // const { BadRequest } = require("../errors");
 // const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   // const { name, email, password } = req.body;
@@ -31,12 +31,14 @@ const register = async (req, res) => {
   const user = await User.create({ ...req.body });
 
   // generate token => since it can be decoded, do not save credentials here
-  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
-    expiresIn: "30d",
-  });
+  // const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+  //   expiresIn: "30d",
+  // });
 
   // front-end could decode this
-  res.status(StatusCodes.CREATED).json({ token });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user: { name: user.getName() }, token: user.createJWT() });
 };
 
 const login = async (req, res) => {
