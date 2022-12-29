fb46383b9e46f6b425dc4d43e94aba7c22b922d4
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-29 14:34:56 +0900

    secutiy packages setup

--- 6-jobs-api/app.js
+++ 6-jobs-api/app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -7,7 +14,17 @@ const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1); // express-rate-limit: If you are behind a proxy/load balancer (usually the case with most hosting services, e.g. Heroku, Bluemix, AWS ELB, Nginx, Cloudflare, Akamai, Fastly, Firebase Hosting, Rackspace LB, Riverbed Stingray, etc.)
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // connect DB
 const connectDB = require("./db/connect");
 
