11d12e5b878698b56bb5dac5d3d0459b81f9a8be
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-29 12:11:59 +0900

    custom mongoose error

--- 6-jobs-api/middleware/error-handler.js
+++ 6-jobs-api/middleware/error-handler.js
@@ -1,10 +1,26 @@
 const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
+  console.log(err);
+
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "something went wrong try again later",
+  };
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  // console.log the mongoose error to set this up
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = 400;
+  }
+  // anything other than custom error are essentially errors emnating from mongoose
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
