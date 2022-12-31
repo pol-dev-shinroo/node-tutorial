54b5d0f98da2d9668c026f20219e916009d71cf9
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-31 10:54:25 +0900

    upload image

--- 7-file-upload/controllers/productController.js
+++ 7-file-upload/controllers/productController.js
@@ -2,7 +2,9 @@ const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 
 const createProduct = async (req, res) => {
-  res.send("create product");
+  // res.send("create product");
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json({ product });
 };
 
 const getAllProduct = async (req, res) => {
--- 7-file-upload/controllers/uploadsController.js
+++ 7-file-upload/controllers/uploadsController.js
@@ -1,6 +1,18 @@
+const path = require("path");
+
 const { StatusCodes } = require("http-status-codes");
 
 const uploadProductImage = async (req, res) => {
+  const productImage = req.files.image;
+  console.log(productImage);
+
+  const imagePath = path.join(
+    __dirname,
+    `../public/uploads/` + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+
   res.send("upload product");
 };
 
