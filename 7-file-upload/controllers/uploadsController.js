f7db169fc3d863c51fd7ed807dd2dd11369aaf1d
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-31 11:23:56 +0900

    additional checks

--- 7-file-upload/controllers/uploadsController.js
+++ 7-file-upload/controllers/uploadsController.js
@@ -1,10 +1,35 @@
 const path = require("path");
-
 const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
 
 const uploadProductImage = async (req, res) => {
+  // additional checks
+  // 1) check if files exists
+  // 2) check format
+  // 3) check size
+
+  console.log(req.files);
+  // 1) check if files exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No file uploaded");
+  }
+
   const productImage = req.files.image;
   console.log(productImage);
+  // 2) check format
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please upload Image");
+  }
+
+  // 3) check size
+
+  const maxSize = 1024 * 1024; // 1kb = 1000, 1mb = 1024 *1024
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      "Please upload image smaller than 1KB"
+    );
+  }
 
   const imagePath = path.join(
     __dirname,
