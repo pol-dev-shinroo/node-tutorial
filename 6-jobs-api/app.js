b752626d723d0a6f6908e546d7439d25d9b290cc
pol-dev-shinroo <manus.dream.language@gmail.com>
2022-12-31 09:46:16 +0900

    swagger implementation

--- 6-jobs-api/app.js
+++ 6-jobs-api/app.js
@@ -7,8 +7,21 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDoument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
+
+// connect DB
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
+
+// routes
+const authRouter = require("./routes/auth");
+const jobRouter = require("./routes/jobs");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -25,14 +38,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-// connect DB
-const connectDB = require("./db/connect");
 
-const authenticateUser = require("./middleware/authentication");
+app.get("/", (req, res) => {
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoument));
 
-// routes
-const authRouter = require("./routes/auth");
-const jobRouter = require("./routes/jobs");
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 
